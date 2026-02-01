c461497b55f6efbde960b52101aff2123500d15f
deepakdotdevs <deepak.06934@gmail.com>
2026-02-01 21:15:57 +0530

    Room Independencies #build.gradle.kts (app level)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.9.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.7")
 
+    // For Room Database
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-compiler:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
 }
