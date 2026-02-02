82facd655ad87465fb8c0f0856ef5ca91c1f98ac
deepakdotdevs <deepak.06934@gmail.com>
2026-02-02 15:16:27 +0530

    BaseRepository #Date -> #BaseRepository

---
+++ app/src/main/java/com/krmu/nexus/data/repository/BaseRepository.kt
@@ -0,0 +1,6 @@
+package com.krmu.nexus.data.repository
+
+open class BaseRepository {
+    // FireBase and Room database added there
+    // Shared Data source logic
+}
--- app/src/main/java/com/krmu/nexus/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/krmu/nexus/viewmodel/BaseViewModel.kt
@@ -3,4 +3,5 @@ import androidx.lifecycle.ViewModel
 open class BaseViewModel : ViewModel() {
     // This is the Parent of All ViewModels and here, we initialise the very Common
     // Logics like loading state, error handling etc
+    // Share Logic for all ViewModels
 }
