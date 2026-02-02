9ec692d1e1bcbbe5a30446692cea1500713a8c79
deepakdotdevs <deepak.06934@gmail.com>
2026-02-02 15:04:36 +0530

    Parent of ViewModel #BaseViewModel

---
+++ app/src/main/java/com/krmu/nexus/viewmodel/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.krmu.nexus.viewmodel
+import androidx.lifecycle.ViewModel
+open class BaseViewModel : ViewModel() {
+    // This is the Parent of All ViewModels and here, we initialise the very Common
+    // Logics like loading state, error handling etc
+}
