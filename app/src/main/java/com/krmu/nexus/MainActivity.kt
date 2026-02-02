6bca6d998d476fb8bbc8df75a3b7fe38fdc26400
deepakdotdevs <deepak.06934@gmail.com>
2026-02-02 14:52:19 +0530

    MainActivity #SetContentView

--- app/src/main/java/com/krmu/nexus/MainActivity.kt
+++ app/src/main/java/com/krmu/nexus/MainActivity.kt
@@ -16,17 +16,7 @@ import com.krmu.nexus.ui.theme.KRMUNexusTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KRMUNexusTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
