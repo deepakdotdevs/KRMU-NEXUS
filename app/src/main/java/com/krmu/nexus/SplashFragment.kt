aa770b68987a60d51cfb5f08f62c13d835d86b7f
deepakdotdevs <deepak.06934@gmail.com>
2026-02-03 22:03:44 +0530

    SplashScreen #Fragment SplashScreen

--- app/src/main/java/com/krmu/nexus/SplashFragment.kt
+++ app/src/main/java/com/krmu/nexus/SplashFragment.kt
@@ -1,10 +1,14 @@
-package com.krmu.nexus
+package com.krmu.nexus.ui.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.se.omapi.Session
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.krmu.nexus.R
+import com.krmu.nexus.databinding.FragmentSplashBinding
+import com.krmu.nexus.utils.SessionManager
+import kotlinx.coroutines.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,25 +20,52 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SplashFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SplashFragment : Fragment() {
+class SplashFragment : Fragment(R.layout.fragment_splash) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentSplashBinding?= null
+    private val binding get() = _binding!!
+    private lateinit var sessionManager: SessionManager
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSplashBinding.bind(view)
+        sessionManager = SessionManager(requireContext())
+        startAnimation()
+        navigateNext()
+    }
+    private fun startAnimation() {
+        binding.logoImage.alpha = 0f
+        binding.logoImage.scaleX = 0.8f
+        binding.logoImage.scaleY = 0.8f
+
+        binding.logoImage.animate()
+            .alpha(1f)
+            .scaleX(1f)
+            .scaleY(1f)
+            .setDuration(1200)
+            .start()
+    }
+    private fun navigateNext() {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            if(sessionManager.isLoggedIn()) {
+                val role = sessionManager.getRole()
+                if(role=="teacher") {
+                    findNavController().navigate(R.id.action_splash_to_teacherDashboard)
+                } else {
+                    findNavController().navigate(R.id.action_splash_to_studentDashboard)
+                }
+            } else {
+                findNavController().navigate(R.id.action_splash_to_roleSelection)
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
