6852ac1124ec06296c1b60522a080243635af1fc
deepakdotdevs <deepak.06934@gmail.com>
2026-02-08 12:46:46 +0530

    RoleSelectionFragment

--- app/src/main/java/com/krmu/nexus/ui/role/RoleSelectionFragment.kt
+++ app/src/main/java/com/krmu/nexus/ui/role/RoleSelectionFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.krmu.nexus.R
+import com.krmu.nexus.databinding.FragmentRoleSelectionBinding
+import java.security.AuthProvider
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,40 +25,30 @@ class RoleSelectionFragment : Fragment(R.layout.fragment_role_selection) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentRoleSelectionBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: AuthViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentRoleSelectionBinding.bind(view)
+        viewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+
+        binding.btnStudent.setOnClickListener {
+            viewModel.setRole("student")
+            val action = RoleSelectionFragmentDirections.actionRoleSelectionToLogin("student")
+            findViewController().navigate(action)
         }
+        binding.btnTeacher.setOnClickListener {
+            viewModel.setRole("teacher")
+            val action = RoleSelectionFragmentDirections.actionRoleSelectionToLogin("teacher")
+            findViewController().navigate(action)
+        }
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_role_selection, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RoleSelectionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RoleSelectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
