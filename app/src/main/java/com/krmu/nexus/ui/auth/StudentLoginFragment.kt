0983efbdcce595814bc58038db54e569535a056d
deepakdotdevs <deepak.06934@gmail.com>
2026-02-09 22:34:52 +0530

    StudentLoginFragment #Login Logic

--- app/src/main/java/com/krmu/nexus/ui/auth/StudentLoginFragment.kt
+++ app/src/main/java/com/krmu/nexus/ui/auth/StudentLoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.krmu.nexus.R
+import com.krmu.nexus.databinding.FragmentStudentLoginBinding
+import com.krmu.nexus.viewmodel.AuthViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,44 +20,29 @@ private const val ARG_PARAM2 = "param2"
  * Use the [StudentLoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class StudentLoginFragment : Fragment() {
+class StudentLoginFragment : Fragment(R.layout.fragment_student_login) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentStudentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: AuthViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentStudentLoginBinding.bind(view)
+        viewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+
+        binding.btnLoginStudent.setOnClickListener {
+            val email = binding.etStudentEmail.text.toString()
+            val password = binding.etStudentPassword.text.toString()
+            viewModel.login(email, password)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_login, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentLoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentLoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
