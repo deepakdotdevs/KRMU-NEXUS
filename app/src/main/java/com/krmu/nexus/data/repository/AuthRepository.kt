0d69b589bcfe7bdb5aadde7faa3a1fe3c8af242e
deepakdotdevs <deepak.06934@gmail.com>
2026-02-16 11:00:53 +0530

    Updated #AuthRepository

--- app/src/main/java/com/krmu/nexus/data/repository/AuthRepository.kt
+++ app/src/main/java/com/krmu/nexus/data/repository/AuthRepository.kt
@@ -3,21 +3,30 @@ package com.krmu.nexus.data.repository
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.tasks.await
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AuthRepository {
 
     private val auth = FirebaseAuth.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
 
-    suspend fun loginUser(email: String, password: String): Result<String> {
+    suspend fun loginUser(email: String, password: String): Result<Pair<String, String>> {
         return try {
             val result = auth.signInWithEmailAndPassword(email, password).await()
             val uid = result.user?.uid ?: ""
-            Result.success(uid)
+
+            val document = firestore.collection("users")
+                .document(uid)
+                .get()
+                .await()
+
+            val role = document.getString("role") ?: "student"
+
+            Result.success(Pair(uid, role))
+
         } catch (e: Exception) {
-            Log.e("LOGIN_ERROR", "Firebase login failed: ${e.message}", e)
             Result.failure(e)
         }
     }
-
 }
 
