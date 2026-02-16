6f4e6408108d6afcbdcc838204d7cf63adbd7ea8
deepakdotdevs <deepak.06934@gmail.com>
2026-02-16 10:30:52 +0530

    #Update -> #SessionManager.kt

--- app/src/main/java/com/krmu/nexus/utils/SessionManager.kt
+++ app/src/main/java/com/krmu/nexus/utils/SessionManager.kt
@@ -3,12 +3,15 @@ package com.krmu.nexus.utils
 import android.content.Context
 class SessionManager(context: Context) {
     private val prefs = context.getSharedPreferences("krmu_session", Context.MODE_PRIVATE)
-    fun saveLogin(role: String) {
+    fun saveUserSession(uid: String, role: String) {
         prefs.edit().putBoolean("isLoggedIn", true)
+            .putString("uid", uid)
             .putString("role", role)
             .apply()
     }
     fun isLoggedIn(): Boolean = prefs.getBoolean("isLoggedIn", false)
+    fun getUserId(): String? = prefs.getString("uid",null)
+
     fun getRole(): String? = prefs.getString("role", null)
     fun logout() {
         prefs.edit().clear().apply()
