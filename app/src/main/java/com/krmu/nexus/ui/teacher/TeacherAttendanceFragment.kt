b0548549dfea576d404afcabf45cca2e7ee26b9e
deepakdotdevs <deepak.06934@gmail.com>
2026-03-13 22:07:06 +0530

    #Bottom Nav Working, #Fragment Switching smoothly #Teacher login -> Dashboard

--- app/src/main/java/com/krmu/nexus/ui/teacher/TeacherAttendanceFragment.kt
+++ app/src/main/java/com/krmu/nexus/ui/teacher/TeacherAttendanceFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.krmu.nexus.R
-import com.krmu.nexus.databinding.FragmentTeacherDashboardBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,15 +14,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TeacherDashboardFragment.newInstance] factory method to
+ * Use the [TeacherAttendanceFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TeacherDashboardFragment : Fragment(R.layout.fragment_teacher_dashboard) {
+class TeacherAttendanceFragment : Fragment(R.layout.fragment_teacher_attendance) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    private lateinit var binding: FragmentTeacherDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -32,24 +30,12 @@ class TeacherDashboardFragment : Fragment(R.layout.fragment_teacher_dashboard) {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentTeacherDashboardBinding.bind(view)
-        loadFragment(TeacherHomeFragment())
-        binding.bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.nav_home -> loadFragment(TeacherHomeFragment())
-                R.id.nav_attendance -> loadFragment(TeacherAttendanceFragment())
-                R.id.nav_schedule -> loadFragment(TeacherScheduleFragment())
-                R.id.nav_profile -> loadFragment(TeacherProfileFragment())
-            }
-            true
-        }
-    }
-    private fun loadFragment(fragment: Fragment) {
-        childFragmentManager.beginTransaction()
-            .replace(R.id.teacherContainer, fragment)
-            .commit()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_teacher_attendance, container, false)
     }
 
     companion object {
@@ -59,12 +45,12 @@ class TeacherDashboardFragment : Fragment(R.layout.fragment_teacher_dashboard) {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TeacherDashboardFragment.
+         * @return A new instance of fragment TeacherAttendanceFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TeacherDashboardFragment().apply {
+            TeacherAttendanceFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
